2d6a66ec523522c96beda05c530c91f594146da6
Sundreesoro <104556116+Sundreesoro@users.noreply.github.com>
2026-06-10 02:18:50 +0800

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
-// We are putting your local database settings directly here to ensure Node can read them!
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'gym_tracker',
-  password: 'jemjem181012', // 👈 Type your actual pgAdmin password here inside quotes
+  password: 'jemjem181012', 
   port: 5432,
 });
 
